6ae549a5b903569df4958caf80aa9aaf4777e156
khams <torresvincent5@gmail.com>
2018-09-11 16:58:55 +0200

    Différentes modifs
    - Ajout d'objets (train et fusée jupiter)
    - Tentative infructueuse de faire des objets qui se baladent dans l'espace
    - Les satellites s'orientent en fonction de leur direction
    - Système de particules avec la planète Rasta

--- planets/Assets/Scripts/InteractableItemBehaviour.cs
+++ planets/Assets/Scripts/InteractableItemBehaviour.cs
@@ -10,6 +10,7 @@ public class InteractableItemBehaviour : MonoBehaviour
 
 	[Header("Variables")]
 	[SerializeField] private float force = 5f;
+	[SerializeField] private float forceRotation = 0f;
 
 	void Awake ()
 	{
@@ -25,6 +26,9 @@ public class InteractableItemBehaviour : MonoBehaviour
 	            // Vecteur légèrement en hauteur
 	            Vector3 shootDirection = (contact.normal + transform.up).normalized;
 	            rb.AddForce(shootDirection * force, ForceMode.Impulse);
+
+	            Vector3 shootRotation = transform.up * forceRotation;
+	            rb.AddTorque(shootRotation, ForceMode.Impulse);
 	        }
 		}
 	}
